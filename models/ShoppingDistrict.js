27d79790ab0416d65e631f56b12aa09b6fad5fe8
howardnguyen714 <howardnguyen714@gmail.com>
2021-04-06 12:15:18 -0700

    added ShoppingDistrict model

---
+++ models/ShoppingDistrict.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const HotelSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  imageUrl: String,
+  tags: [{
+    tagName: String,
+    upvotes: Number
+  }],
+  tips: [{
+    tipName: String,
+    upvotes: Number
+  }],
+  dinings: [{
+    type: mongoose.Types.Schema.ObjectId,
+    ref: 'Dining'
+  }],
+  shops: [{
+    type: mongoose.Types.Schema.ObjectId,
+    ref: 'Shop'
+  }],
+  shows: [{
+    type: mongoose.Types.Schema.ObjectId,
+    ref: 'Show'
+  }]
+})
+
+const Hotel = mongoose.model('Hotel', HotelSchema)
+module.exports = Hotel
