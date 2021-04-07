9c3176af036f4cb056fcf4d766e767bd14453b06
howardnguyen714 <howardnguyen714@gmail.com>
2021-04-06 21:34:03 -0700

    updated controllers/index.js to list all other contollers

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,12 @@
 module.exports = {
-    games: require('./games'),
+    dining: require('./dining'),
+    hotel: require('./hotel'),
+    land: require('./land'),
+    park: require('./park'),
+    resort: require('./resort'),
+    ride: require('./ride'),
+    shop: require('./shop'),
+    shoppingDistrict: require('./shoppingDistrict'),
+    show: require('./show'),
+    surroundingArea: require('./surroundingArea'),
 };
