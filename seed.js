698ac2e5fac8d22c19ab848fd17dd752ba00f511
howardnguyen714 <howardnguyen714@gmail.com>
2021-04-07 18:33:02 -0700

    modified seed.js to successfully seed parks, resorts, and create associations

--- seed.js
+++ seed.js
@@ -1,13 +1,34 @@
 require("dotenv").config()
 const db = require('./models');
-const data = require('./gameData.json');
+const data = require('./seedData.json');
 
-db.Game.deleteMany({}, (err, deletedGames) => {
-    db.Game.create(data.games, (err, seededGames) => {
-        if (err) console.log(err);
-        
-        console.log(data.games.length, 'games created successfully');
-        
-        process.exit();
-    });
+db.Resort.deleteMany({}, (err, deletedResorts) => {
+    db.Park.deleteMany({}, (err, deletedParks) => {
+        db.Resort.create(data.resorts, (err, seededResorts) => {
+            if (err) console.log(err);
+            
+            console.log("==================================================")
+            console.log(data.resorts.length, 'resorts created successfully');
+            console.log(seededResorts)
+            console.log("==================================================")
+            db.Park.create(data.parks, (err, seededParks) => {
+                
+                if (err) console.log(err);
+                
+                console.log("==================================================")
+                console.log(data.parks.length, 'parks created successfully');
+                console.log(seededParks)
+                console.log("==================================================")
+    
+                db.Resort.findById(seededResorts[0]["_id"], (err, foundResort) => {
+                    for (park in seededParks) {
+                        foundResort.parks.push(seededParks[park]["_id"])
+                    }
+                    console.log(foundResort)
+                    foundResort.save()
+                })
+                // process.exit();    
+            })
+        });
+    })
 });
