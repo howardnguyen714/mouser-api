aa12ad703b3bba18f947c5d81c9c6871714bcf19
howardnguyen714 <howardnguyen714@gmail.com>
2021-04-07 18:29:53 -0700

    updated models/index.js to include all models

--- models/index.js
+++ models/index.js
@@ -14,4 +14,13 @@ mongoose.connect(connectionString, configOptions)
 
 module.exports = {
     Resort: require('./Resort'),
+    Park: require('./Park'),
+    // Dining: require('./Dining'),
+    // Hotel: require('./Hotel'),
+    // Land: require('./Land'),
+    // Ride: require('./Ride'),
+    // Shop: require('./Shop'),
+    // ShoppingDistrict: require('./ShoppingDistrict'),
+    // Show: require('./Show'),
+    // SurroundingArea: require('./SurroundingArea'),
 };
